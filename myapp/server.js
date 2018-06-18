2f6cff72e7508bc21be6274b4f7d214e63b3346a
Rosh974 <didier.vilrus1@gmail.com>
2018-06-18 13:45:05 +0400

    modification emplacement server.js

--- myapp/server.js
+++ myapp/server.js
@@ -10,13 +10,23 @@ app.use(express.static('public')); app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/addition', function (req, res) {
-  var nbr1 = req.body.nbr1;
-  console.log(nbr1)
-  var nbr2 = req.body.nbr2;
-  console.log(nbr2)
-  var result = 0;
-  result = parseInt(nbr1) + parseInt(nbr2);
-  res.send('' + result);
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
 
+  app.post('/addition', function (req, res) {
+    var nbr1 = req.body.nbr1;
+    console.log(nbr1)
+    var nbr2 = req.body.nbr2;
+    console.log(nbr2)
+    var result = 0;
+    result = parseInt(nbr1) + parseInt(nbr2);
+    res.send('' + result);
+
+  })
+
+ 
+});
+
+app.listen(3045, function () {
+  console.log('ok');
 })
