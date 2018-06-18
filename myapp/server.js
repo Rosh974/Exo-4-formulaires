471a0f9d4b844a9768b7f313a7cc2272642eed0b
Rosh974 <didier.vilrus1@gmail.com>
2018-06-18 15:50:09 +0400

    modification script avec problème result

--- myapp/server.js
+++ myapp/server.js
@@ -6,26 +6,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public')); app.get('/', function (req, res) {
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
 
-  app.post('/addition', function (req, res) {
-    var nbr1 = req.body.nbr1;
-    console.log(nbr1)
-    var nbr2 = req.body.nbr2;
-    console.log(nbr2)
-    var result = 0;
-    result = parseInt(nbr1) + parseInt(nbr2);
-    res.send('' + result);
+app.post('/addition', function (req, res) {
+  var nbr1 = req.body.nbr1;
+  console.log(nbr1)
+  var nbr2 = req.body.nbr2;
+  console.log(nbr2)
+  var result = 0;
+  result = parseInt(nbr1) + parseInt(nbr2);
+  res.send('' + result);
 
-  })
-
- 
-});
+})
 
 app.listen(3045, function () {
   console.log('ok');
