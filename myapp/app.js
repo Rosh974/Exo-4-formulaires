3c0b5b920a40d018440329e61e5f4db1f4e2027f
Rosh974 <didier.vilrus1@gmail.com>
2018-06-18 11:06:43 +0400

    changement de formulaire

--- myapp/app.js
+++ myapp/app.js
@@ -10,7 +10,17 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/app.js')
+})
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/style.css')
+})
+
 
 app.listen(3045, function () {
   console.log('Listening on port 3045')
 })
+
+
