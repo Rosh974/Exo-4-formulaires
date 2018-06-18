c01abc9e333da3a4382cee5980dd6eb49078f555
Rosh974 <didier.vilrus1@gmail.com>
2018-06-18 13:30:49 +0400

    code finale

---
+++ myapp/public/script.js
@@ -0,0 +1,16 @@
+
+//SOLUTION FINALE
+
+$("#send").click(function () {
+    var nbr1 = $('#nombre1').val();
+    console.log(nbr1)
+    var nbr2 = $('#nombre2').val();
+    console.log(nbr2)
+    var url = "http://localhost:3045/addition"
+    $.post(url, { nbr1: nbr1, nbr2: nbr2 }, function (data) {
+        console.log(data);
+        $('#result').append(data);
+
+    });
+
+});
