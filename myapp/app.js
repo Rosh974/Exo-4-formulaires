88ed8a289da97420f92128a47bb60b7d5a009566
Rosh974 <didier.vilrus1@gmail.com>
2018-06-18 10:51:21 +0400

    ajout du formulaire et afficher sur le serveur

---
+++ myapp/app.js
@@ -0,0 +1,16 @@
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+app.listen(3045, function () {
+  console.log('Listening on port 3045')
+})
