ab3dac856458f5df98d0fb47a308e384ae45c212
Rosh974 <didier.vilrus1@gmail.com>
2018-06-19 08:21:25 +0400

    correction avec ajout de document ready function

--- myapp/public/script.js
+++ myapp/public/script.js
@@ -1,18 +1,21 @@
 
 //SOLUTION FINALE
-
-$("#send").click(function () {
-    var nbr1 = $('#nombre1').val();
-    console.log(nbr1)
-    var nbr2 = $('#nombre2').val();
-    console.log(nbr2)
+$(function(){
     
-    var url = "http://localhost:3045/addition"
+    $("#send").click(function () {
+        
+        var nbr1 = $('#nombre1').val();
+        console.log(nbr1)
+        var nbr2 = $('#nombre2').val();
+        console.log(nbr2)
+        
+        var url = "http://localhost:3045/addition"
+        
+        $.post(url, { nbr1: nbr1, nbr2: nbr2 }, function (data) {
+            console.log(data);
+            $('#result').append(data);
     
-    $.post(url, { nbr1: nbr1, nbr2: nbr2 }, function (data) {
-        console.log(data);
-        $('#result').append(data);
-
+        });
+    
     });
-
-});
+})
